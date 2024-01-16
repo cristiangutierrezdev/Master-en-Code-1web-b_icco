88ff9151fce4057dd04dbf67ab2121a2e3c8dcce
tablonimus <lautamartin1995@gmail.com>
2024-01-15 22:41:19 -0300

    JS

---
+++ M2 - JAVASCRIPT/DIA 1 - INTRO/README.md
@@ -0,0 +1,7 @@
+Extensiones de ayuda:
+-Error Lens
+-Console Ninja
+-Prettier
+
+`document.write => para mostrar en pantalla lo expresado entre los parentesis`
+`document.write("Bienvenidos a DEV.F")`
---
+++ M2 - JAVASCRIPT/DIA 1 - INTRO/assets/js/index.js
@@ -0,0 +1,51 @@
+/* 
+Ejemplo uno de comentario / Comentario en bloque => apretando ALT + SHIFT + A
+*/
+// Ejemplo dos de comentario / Comentario en linea => utilizando doble / => apretando CTRL + }
+
+/* Console.log => para mostrar en consola lo expresado entre los parentesis */
+/* Fue hecho para debugear/depurar el código */
+// console.log("Esta es una suma =>", 2 + 2);
+
+/* Alert / Alerta => accion bloqueante 🛑*/
+// alert("El código ha finalizado, muchas gracias!")
+
+/* -----------------------VARIABLES Y CONSTANTES-------------------------- */
+/* Las variables pueden ser definidas con la palabra reservada "var" o "let"/  constante=> const */
+// DECLARACIÓN (darle nombre)y ASIGNACIÓN DE VALOR(darle valor) de una variable
+
+let nombre = "Lautaro"; //=> variable con let
+//nombre = "Rodrigo" //=> Redeclaración/Reasignación de un valor
+const apellido = "Martin"; //=> una constante
+
+// console.log("Este es mi nombre=> ", nombre);
+// console.log("Este es mi apellido=> ", apellido);
+// console.log(nombre + " " + apellido);
+
+/* -----------------------TIPOS DE DATOS------------------------- */
+//undefined => aún no está definido /  su valor NO existe
+let nullValue = null; //=> su valor es nulo
+let string = "Cadena de caracteres";
+let number = 5;
+let floatNumber = 1.5; //=> Número decimal va SIEMPRE con .
+let boolean = true;
+let boolean2 = false;
+
+/* -----------------------OPERADORES MATEMÁTICOS------------------------- */
+/* Las operaciones matemáticas arrojan un valor de salida único */
+let numeroFavorito = 22;
+
+let suma = numeroFavorito + 2; //24
+
+let resta = 5 - 5; // 0
+
+let multipliacion = 5 * 5; //25
+
+let division = 25 / 5; //5
+
+let residuoDeDivison = 25 % 5; // 25/5 => 0 sin resto
+
+let esPar = 3 % 2; /* Si da 0 es par, sino es impar */
+
+let potencia = 2 ** 3; // 8
+
