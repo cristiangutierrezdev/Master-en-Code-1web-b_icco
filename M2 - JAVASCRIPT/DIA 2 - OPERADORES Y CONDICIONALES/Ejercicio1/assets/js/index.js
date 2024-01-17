25bbb1936fe2e2a6008fab88a22aa13afab80df8
tablonimus <lautamartin1995@gmail.com>
2024-01-17 20:16:32 -0300

    nuevo ejercicio

---
+++ M2 - JAVASCRIPT/DIA 2 - OPERADORES Y CONDICIONALES/Ejercicio1/assets/js/index.js
@@ -0,0 +1,40 @@
+// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = null;
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = null;
+
+// Crea una variable booleana:
+const nuevoBool = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = null;
+
+// Resuelve el siguiente problema matemático://
+const nuevaMultiplicacion = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % null === 1;
+
+/* ----------------------------------------------------------------------------------- */
+
+// Ejercicio 1: Crear variables de diferentes tipos de datos y luego consologuear su valor.
+// a. Crear una variable llamada "nombre" y asignarle tu nombre como cadena.
+
+// b. Crear una variable llamada "edad" y asignarle tu edad como número.
+
+// c. Crear una variable llamada "esEstudiante" y asignarle un valor booleano que indique si eres estudiante o no.
+
+// d. Crear una variable llamada "nulo" y asignarle el valor nulo.
+
+
+
+
+
+
+/* ----------------------------------------------------------------------------------- */
+// Ejercicio 3: Operaciones matemáticas
+// a. Crear dos variables, "numero1" y "numero2", y asignarles valores numéricos.
+// b. Calcular la suma, resta, multiplicación y división de los dos números (en nuevas variables) y mostrar los resultados en la consola.
