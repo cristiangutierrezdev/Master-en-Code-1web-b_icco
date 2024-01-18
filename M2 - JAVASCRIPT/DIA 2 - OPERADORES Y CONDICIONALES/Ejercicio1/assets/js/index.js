5fd42f4fa537789a32d2e87426be4da09f62dddd
tablonimus <lautamartin1995@gmail.com>
2024-01-17 21:17:16 -0300

    respuestas hechas

--- M2 - JAVASCRIPT/DIA 2 - OPERADORES Y CONDICIONALES/Ejercicio1/assets/js/index.js
+++ M2 - JAVASCRIPT/DIA 2 - OPERADORES Y CONDICIONALES/Ejercicio1/assets/js/index.js
@@ -1,40 +1,51 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
-
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "Hola chicos como estan!?";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 2112;
 
-// Crea una variable booleana:
-const nuevoBool = null;
+// Crea una variable booleana: true or false
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = null;
+const nuevaResta = 155 - 55;
 
 // Resuelve el siguiente problema matemático://
-const nuevaMultiplicacion = null;
+const nuevaMultiplicacion = 155 * 55;
 
-// Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % null === 1;
+// Resuelve el siguiente problema matemático reemplazando null para que el resultado final sea 1:
+const nuevoModulo = 21 % 2 === 1;
 
 /* ----------------------------------------------------------------------------------- */
 
 // Ejercicio 1: Crear variables de diferentes tipos de datos y luego consologuear su valor.
-// a. Crear una variable llamada "nombre" y asignarle tu nombre como cadena.
-
+// a. Crear una variable llamada "nombre" y asignarle tu nombre como cadena/string.
+let nombre = "Lautaro";
 // b. Crear una variable llamada "edad" y asignarle tu edad como número.
-
+let edad = 28;
 // c. Crear una variable llamada "esEstudiante" y asignarle un valor booleano que indique si eres estudiante o no.
+let esEstudiante = false;
+// d. Crear una variable llamada "nulo" y asignarle el valor nulo/null.
+let nulo = null;
 
-// d. Crear una variable llamada "nulo" y asignarle el valor nulo.
-
-
-
-
-
+console.log(nombre, edad, esEstudiante, nulo);
 
 /* ----------------------------------------------------------------------------------- */
 // Ejercicio 3: Operaciones matemáticas
 // a. Crear dos variables, "numero1" y "numero2", y asignarles valores numéricos.
 // b. Calcular la suma, resta, multiplicación y división de los dos números (en nuevas variables) y mostrar los resultados en la consola.
+
+let numero1 = 12;
+let numero2 = 21;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Resultados del ejercicio 3 ======>");
+console.log("Suma", suma);
+console.log("Resta", resta);
+console.log("Multiplicacion", multiplicacion);
+console.log("Division", division);
