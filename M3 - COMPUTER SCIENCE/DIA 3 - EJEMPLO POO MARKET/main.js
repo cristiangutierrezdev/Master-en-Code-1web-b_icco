a26252d3a8528dd8d76f96eac93efba14a019918
tablonimus <lautamartin1995@gmail.com>
2024-03-06 21:33:58 -0300

    market ready

--- M3 - COMPUTER SCIENCE/DIA 3 - EJEMPLO POO MARKET/main.js
+++ M3 - COMPUTER SCIENCE/DIA 3 - EJEMPLO POO MARKET/main.js
@@ -40,10 +40,51 @@ class Celular extends Producto {
     this.imagen = imagen;
   }
 }
+/* -------------------Función que crea tarjetas de productos en el html-------------------- */
+const showProducts = (products) => {
+  let productsHTML = "";
 
+  products.forEach(
+    (product) =>
+      (productsHTML += `      
+                <div class="card" style="width: 18rem">
+                    <img src=${product.imagen} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                      <h5 class="card-title">${product.marca} - ${product.modelo}</h5>
+                      <p class="card-text">Memoria Ram: ${product.ram}</p>
+                      <p class="card-text">Almacenamiento: ${product.almacenamiento}"</p>
+                      <p class="card-text">Precio: $${product.precio}</p>
+                      <a href="#" class="btn btn-primary">🛒 Agregar al carrito</a>
+                    </div>
+                </div>
+                           `)
+  );
 
+  productsContainer.innerHTML = productsHTML;
+};
+
+/* -------------------Función que crea una nueva instancia-------------------- */
+
+const createNewProduct = (product) => {
+  let newProduct = new Celular(
+    product.precio,
+    product.stock,
+    product.marca,
+    product.modelo,
+    product.ram,
+    product.almacenamiento,
+    product.camara,
+    product.imagen
+  );
+
+  products.push(newProduct);
+
+  showProducts(products);
+};
 
 /*-------------------------------- MANEJO DEL DOM ---------------------------------------*/
+const products = [];
+
 let precioInput = document.getElementById("precio");
 let stockInput = document.getElementById("stock");
 let marcaInput = document.getElementById("marca");
@@ -52,10 +93,12 @@ let ramInput = document.getElementById("ram");
 let almacenamientoInput = document.getElementById("almacenamiento");
 let camaraInput = document.getElementById("camara");
 let imagenInput = document.getElementById("imagen");
+let createBtn = document.getElementById("create-btn");
+let productsContainer = document.getElementById("products-container");
+
 let formValues = {}; //Aqui iran los valores del formulario
 
 function onChangeInput(event) {
-///                                 marca       :   samsung
   formValues = { ...formValues, [event.target.id]: event.target.value };
   console.log(formValues);
 }
@@ -83,12 +126,17 @@ ramInput.addEventListener("change", (event) => {
 almacenamientoInput.addEventListener("change", (event) => {
   onChangeInput(event);
 });
+
 camaraInput.addEventListener("change", (event) => {
   onChangeInput(event);
 });
+
 imagenInput.addEventListener("change", (event) => {
   onChangeInput(event);
 });
 
+createBtn.addEventListener("click", (event) => {
+  event.preventDefault(); //evita que la página se recargue
 
-console.log(formValues);
+  createNewProduct(formValues);
+});
