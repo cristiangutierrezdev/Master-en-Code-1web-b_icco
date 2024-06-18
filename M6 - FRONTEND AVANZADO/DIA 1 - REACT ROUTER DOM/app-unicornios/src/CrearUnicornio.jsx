37801971956b4bf56d08a59e36b04a692ac9e7c3
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-06-17 20:36:02 -0500

    Se agrega el componente crear unicornio

--- M6 - FRONTEND AVANZADO/DIA 1 - REACT ROUTER DOM/app-unicornios/src/CrearUnicornio.jsx
+++ M6 - FRONTEND AVANZADO/DIA 1 - REACT ROUTER DOM/app-unicornios/src/CrearUnicornio.jsx
@@ -2,34 +2,56 @@ import { InputGroup, Form, Button } from "react-bootstrap"
 import axios from "axios"
 import { useState, useEffect } from "react"
 
-function CrearUnicornio(){
+    function CrearUnicornio(){
     const url = "https://experimental-unicorns-experimental-org.koyeb.app/api/v1/unicorns"
     const [nombre, setNombre] = useState("")
     const [imagen, setImagen] = useState("")
     const [edad, setEdad] = useState("")
     const [poder, setPoder] = useState("")
+    // const [ nuevoUnicornio , setNuevoUnicornio ] = useState({})
 
     function valorImagen(evento){
-        console.log(evento.target.value);
+        setImagen(evento.target.value);
     }
     function valorNombre(evento){
-        console.log(evento.target.value);
+        setNombre(evento.target.value);
     }
     function valorEdad(evento){
-        console.log(evento.target.value);
+        setEdad(evento.target.value);
     }
     function valorPoder(evento){
-        console.log(evento.target.value);
+        setPoder(evento.target.value);
     }
-    
-    // useEffect(()=>{
-    //     axios.post(url,)
-    // },[])
+    function enviarDatos(){
+        if(nombre  && imagen && edad && poder !== ""){
+           console.log("se creo el unicornio"); 
+           crearUnicornio()
+
+        }else{
+            alert("Completa los campos")
+        }
+    }
+
+    function crearUnicornio(){
+        axios.post(url, {
+            name : nombre,
+            power : poder,
+            age : edad,
+            image : imagen
+        } )
+                .then(res =>{
+                    console.log("Creado con exito")
+     
+                })
+                .catch(error =>{
+                    console.log(error);
+                })
+    }
+  
     return (
         <>
         <InputGroup className="mb-3 mt-3">
-        <Form.Control
-        onChange={valorImagen}       
+        <Form.Control onChange={valorImagen}       
           placeholder="Url de la imagen"
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
@@ -59,7 +81,7 @@ function CrearUnicornio(){
           aria-describedby="basic-addon2"
         />
       </InputGroup>
-      <Button onClick={()=>{obtenerValorInputs(e)}}>Crear Unicornio</Button>
+      <Button onClick={enviarDatos} >Crear Unicornio</Button>
       </>
     )
 }
