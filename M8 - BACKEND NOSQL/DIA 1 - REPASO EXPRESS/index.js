46e48f02252c9fbbc400eb0c08ef16dbc4b28f6c
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-08-26 20:47:12 -0500

    se agrega la autenticacion dinamica

--- M8 - BACKEND NOSQL/DIA 1 - REPASO EXPRESS/index.js
+++ M8 - BACKEND NOSQL/DIA 1 - REPASO EXPRESS/index.js
@@ -1,29 +1,36 @@
 import express from "express";
 
-const app = express();
+const users = [ "john", "jane", "pepito", "juanito"]
 
-function autenticacion (req,res,next){
-    console.log(`Metodo : ${req.method}`);
-    const autenticacion = req.query.userName;
-  
-    if (autenticacion === "jane") {
-      next();
-    } else {
-        res.send("<h2>No estas autenticado en la base de datos</h2>");
+const app = express();
+app.use(express.json())
+
+function autenticacion (req, res, next){
+    console.log(`Query: ${req.query.userName}`);
+    const nombreUsuario = req.query.userName
+    console.log(`Metodo: ${req.method}`);
+
+    const usuarios = users.find((usuario)=>{
+        return nombreUsuario === usuario
+    })
+    console.log(usuarios);
+    if(usuarios === nombreUsuario ){
+        next()
+    }else{
+        res.status(401).sendStatus(401)
     }
 }
 
-app.get("/api/v1/unicorns", function (peticion, respuesta) {
-  console.log(peticion.params);
-  respuesta.send("<h2>Pagina principal</h2>");
-});
+app.get("/", function (peticion, respuesta) {
+    respuesta.send("<h2>Pagina principal</h2>");
+  });
 
-app.use(autenticacion);
+app.use(autenticacion)
 
 app.get("/:id", function (peticion, respuesta) {
     const nombreUsuario = peticion.query.userName
   respuesta.send(`<p>id usuario: ${peticion.params.id}</p> 
-                  <p>Nombre: ${nombreUsuario}</p>`);
+                  <p>Nombre:<b> ${nombreUsuario}</b></p>`);
 });
 
 app.listen(3000, () => {
--- M8 - BACKEND NOSQL/DIA 1 - REPASO EXPRESS/readme.mk
+++ M8 - BACKEND NOSQL/DIA 1 - REPASO EXPRESS/readme.mk
@@ -1,8 +1,10 @@
 # Middleware de autenticación básica
 
-Objetivo: Implementar un middleware que verifique si el usuario está autenticado antes de permitir el acceso a ciertas rutas.
-Pasos:
-Crea una aplicación básica con Express.
-Define un middleware que verifique si una clave de autenticación (req.query.user) está presente y es válida.
-Si la clave es válida, permite el acceso a la ruta; de lo contrario, responde con un mensaje de error y un código de estado 401 (no autorizado).
-Aplica este middleware a rutas protegidas como /profile
+### Objetivo:
+ Implementar un middleware que verifique si el usuario está autenticado antes de permitir el acceso a ciertas rutas.
+### Pasos:
+-Crea una aplicación básica con Express.
+-Define un middleware que verifique si una clave de autenticación ``(req.query.user)`` está presente y es válida.
+-Si la clave es válida, permite el acceso a la ruta; de lo contrario, responde con un mensaje de error y un código de estado 401 (no autorizado).
+ Aplica este middleware a rutas protegidas como ```/profile```
+<!-- @import "[TOC]" {cmd="toc" depthFrom=1 depthTo=6 orderedList=false} -->
