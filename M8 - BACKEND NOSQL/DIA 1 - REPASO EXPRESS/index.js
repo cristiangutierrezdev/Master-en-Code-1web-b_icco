40fffcf041c9dcc93be98e16a9d9cc4a7a713530
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-08-26 20:12:06 -0500

    Se agrega la autenticacion del middelware

--- M8 - BACKEND NOSQL/DIA 1 - REPASO EXPRESS/index.js
+++ M8 - BACKEND NOSQL/DIA 1 - REPASO EXPRESS/index.js
@@ -1,28 +1,31 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(function(peticion, respuesta, next){
-    console.log(`Metodo : ${peticion.method}`);
-    console.log("esto es antes de llegar al servidor");
-    next()
-})
+function autenticacion (req,res,next){
+    console.log(`Metodo : ${req.method}`);
+    const autenticacion = req.query.userName;
+  
+    if (autenticacion === "jane") {
+      next();
+    } else {
+        res.send("<h2>No estas autenticado en la base de datos</h2>");
+    }
+}
 
-app.get('/', function(peticion, respuesta){
-    console.log(peticion.params);
-    respuesta.send("<h2>Pagina principal</h2>")
-}) 
+app.get("/api/v1/unicorns", function (peticion, respuesta) {
+  console.log(peticion.params);
+  respuesta.send("<h2>Pagina principal</h2>");
+});
 
+app.use(autenticacion);
 
-app.get('/:id', function(peticion, respuesta){
-    console.log(peticion.query);
-    respuesta.send(`<p>Usuario ${peticion.params.id}</p>`)
-}) 
+app.get("/:id", function (peticion, respuesta) {
+    const nombreUsuario = peticion.query.userName
+  respuesta.send(`<p>id usuario: ${peticion.params.id}</p> 
+                  <p>Nombre: ${nombreUsuario}</p>`);
+});
 
-
-
-app.listen(3000, ()=>{
-    console.log("Servidor corriendo en el puerto " + 3000);
-})
-
-
+app.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto " + 3000);
+});
