f990ec86a3f060d30df83b4c39e0281d3609a387
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-08-26 20:12:52 -0500

    agrego el readme del ejercicio

---
+++ M8 - BACKEND NOSQL/DIA 1 - REPASO EXPRESS/readme.mk
@@ -0,0 +1,8 @@
+# Middleware de autenticación básica
+
+Objetivo: Implementar un middleware que verifique si el usuario está autenticado antes de permitir el acceso a ciertas rutas.
+Pasos:
+Crea una aplicación básica con Express.
+Define un middleware que verifique si una clave de autenticación (req.query.user) está presente y es válida.
+Si la clave es válida, permite el acceso a la ruta; de lo contrario, responde con un mensaje de error y un código de estado 401 (no autorizado).
+Aplica este middleware a rutas protegidas como /profile
